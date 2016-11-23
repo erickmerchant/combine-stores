977137ed489fd331bf7f6b2aa46c47d3b3f3a888
Erick Merchant <erickmerchant@gmail.com>
2016-11-22 20:28:40 -0800

    Upgrade nyc

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-var test = require('tape')
-
-test('', function (t) {
-  t.plan(1)
-
-  t.ok(true)
-})
+// var test = require('tape')
+//
+// test('', function (t) {
+//   t.plan(1)
+//
+//   t.ok(true)
+// })
